8dbaef01851b2c5663fdc3f1588c1781009cf8c4
aComputerWhitch <RainaRBaron@gmail.com>
2021-04-29 17:25:27 -0400

    timmer

---
+++ timer.js
@@ -0,0 +1,34 @@
+const timer = document.querySelector('#time');
+const reset_btn = document.querySelector('#reset_btn');
+
+let time = 0,
+  interval;
+
+function showTime() {
+  time += 1;
+  timer.innerHTML = toHHMMSS(time);
+}
+
+function start() {
+  interval = setInterval(showTime, 1000);
+}
+
+function reset() {
+  clearInterval(interval);
+  interval = setInterval(showTime, 1000);
+  //pause_btn.innerHTML = 'PAUSE';
+  time = 0;
+  timer.innerHTML = toHHMMSS(time);
+}
+
+function toHHMMSS(time) {
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time - hours * 3600) / 60);
+  let seconds = time - hours * 3600 - minutes * 60;
+
+  hours = `${hours}`.padStart(2, '0');
+  minutes = `${minutes}`.padStart(2, '0');
+  seconds = `${seconds}`.padStart(2, '0');
+
+  return hours + ':' + minutes + ':' + seconds;
+}
